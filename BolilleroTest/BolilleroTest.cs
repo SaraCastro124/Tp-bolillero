b98b1f3fb9f683155c64a8b88ceebd7baa3efe17
brenda20-22 <duranpbrenda@gmail.com>
2023-05-04 17:30:28 -0300

    test

--- BolilleroTest/BolilleroTest.cs
+++ BolilleroTest/BolilleroTest.cs
@@ -4,13 +4,19 @@ namespace BolilleroTest;
 
 public class BolilleroTest
 {
-    public Bolillero bolillero {get; set;}
+    public Bolillero bolillero { get; set; }
     public BolilleroTest() => bolillero = new Bolillero(10, new ElegirPrimera());
 
     [Fact]
     public void SacarBolilla()
     {
         var bolilla = bolillero.SacarBolilla();
-        Assert.Equal()
+        Assert.Equal(0, bolilla);
+        Assert.Equal(9, bolillero.Bolillas.Count);
+        Assert.Equal(1, bolillero.Afuera.Count);
     }
-}
+    [Fact]
+    public void MeterBolillas()
+    {
+        var bolilla = bolillero.MeterBolillas();
+    }
