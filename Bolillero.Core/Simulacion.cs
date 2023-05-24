f20eb43f64915d4fcb3268a25897f2b61bd85ad7
SaraCastro124 <sara1330castro@gmail.com>
2023-05-24 15:43:23 -0300

    N

--- Bolillero.Core/Simulacion.cs
+++ Bolillero.Core/Simulacion.cs
@@ -1,11 +1,12 @@
-namespace Bolillero.Core
+namespace BolilleroCore
 {
     public class Simulacion
     {
 
-        public void SimularSinHilos()
-        {
+        public long SimularSinHilos(Bolillero bolillero, List<int> jugada, int cantidadSim)
+            => bolillero.JugarNVeces(jugada, cantidadSim);
 
-        }
+        public long SimularConHilos(Bolillero bolillero, List<int> jugada, int cantidadSim, int cantidadHilos)
+            => bolillero.JugarNVeces();
     }
 }
