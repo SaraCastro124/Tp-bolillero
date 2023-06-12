a9571c0c122381e2a7ee17fcb3d1c7711f26c8cc
SaraCastro124 <sara1330castro@gmail.com>
2023-06-12 17:12:39 -0300

    actualitation de test

--- BolilleroTest/SimulacionTest.cs
+++ BolilleroTest/SimulacionTest.cs
@@ -1,17 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
+using BolilleroCore;
 
-namespace BolilleroTest
+namespace BolilleroTest;
+
+public class SimulacionTest
 {
-    public class SimulacionTest
+    private Bolillero _bolillero;
+    public Simulacion Simulacion { get; set; }
+    public List<int> _jugada;
+    private int _cantidadSim;
+    private int _hilos;
+
+    public SimulacionTest()
     {
-        [Fact]
-        public void Test1()
-        {
-            Assert.True(true);
-        }
+        this._bolillero = new Bolillero(10, new ElegirPrimera());
+        this.Simulacion = new Simulacion();
+        _jugada = new List<int>() { 1 };
+        _cantidadSim = 1;
+        _hilos = 2;
     }
+
+    [Fact]
+    public void SimularSinHilosTest()
+    {
+        var ganadas = Simulacion.SimularSinHilos(_bolillero, _jugada, _hilos);
+        Assert.Equal<long>(1, ganadas);
+    }
+
+    [Fact]
+
+    public void SimularConHilos()
+    {
+
+    }
+
 }
