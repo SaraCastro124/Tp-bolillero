a5e9794f2713b6961736296012dd92b204571941
SaraCastro124 <sara1330castro@gmail.com>
2023-06-26 16:41:44 -0300

    actualitation

--- BolilleroTest/SimulacionTest.cs
+++ BolilleroTest/SimulacionTest.cs
@@ -35,10 +35,10 @@ public class SimulacionTest
     }
 
     [Fact]
-    public void SimularConHilosAsync()
+    public async Task SimularConHilosAsync()
     {
-        var ganaAsync = Simulacion.SimularConHilosAsync(_bolillero, _jugada, _cantidadSim, _hilos);
-        Assert.Equals(_hilos, _cantidadSim);
+        var ganaAsync = await Simulacion.SimularConHilosAsync(_bolillero, _jugada, _cantidadSim, _hilos);
+        Assert.Equal(_hilos, ganaAsync);
     }
 
 }
