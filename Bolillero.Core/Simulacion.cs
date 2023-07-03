b5e37cbfc60e7131f4c4181860509124a84306ec
SaraCastro124 <sara1330castro@gmail.com>
2023-07-03 16:42:32 -0300

    async

--- Bolillero.Core/Simulacion.cs
+++ Bolillero.Core/Simulacion.cs
@@ -29,6 +29,22 @@ namespace BolilleroCore
             await Task<long>.WhenAll(tareasAsync);
             return tareasAsync.Sum(t => t.Result);
         }
+        public async Task<long> SimularParallelAsync(Bolillero bolillero, List<int> jugada, int cantidadSim, int cantidadHilos)
+        {
+            long[] resultados = new long[cantidadHilos]; 
+
+            await Task.Run(() =>
+            Parallel.For(0,
+                        cantidadHilos,
+                        i => {
+                                var clon = bolillero.Clonar();
+                                resultados[i] = 
+                                } 
+                );
+            );
+            return resultados.Average();
+
+        }
 
     }
 }
